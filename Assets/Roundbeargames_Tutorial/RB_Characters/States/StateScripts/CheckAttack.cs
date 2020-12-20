73d0f2397780bd053804bde37b7147760f5c1dfa
19449703 <19449703@163.com>
2020-12-20 14:05:30 +0800

    3

---
+++ Assets/Roundbeargames_Tutorial/RB_Characters/States/StateScripts/CheckAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace roundbeargames_tutorial
+{
+    [CreateAssetMenu(fileName = "CheckAttack", menuName = "Roundbeargames/AbilityData/CheckAttack")]
+    public class CheckAttack : StateData
+    {
+        public override void OnEnter(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            
+        }
+
+        public override void UpdateAbility(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+            CharacterControl control = characterState.GetCharacterControl(animator);
+
+            if (control.attack)
+            {
+                animator.SetBool(TransitionParameter.Attack.ToString(), true);
+            }
+        }
+
+        public override void OnExit(CharacterState characterState, Animator animator, AnimatorStateInfo stateInfo)
+        {
+        }
+    }
+}
