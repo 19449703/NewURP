81f09aabb9f19afb687d994d68d1186f43687213
武珂 <kew@taplion.com>
2020-12-26 15:49:28 +0800

    19

--- Assets/Roundbeargames_Tutorial/RB_Characters/PathFindingAgent/PathFindingAgent.cs
+++ Assets/Roundbeargames_Tutorial/RB_Characters/PathFindingAgent/PathFindingAgent.cs
@@ -11,6 +11,12 @@ namespace roundbeargames_tutorial
         public GameObject target;
         NavMeshAgent navMeshAgent;
 
+        Coroutine move;
+        public Vector3 startPosition;
+        public Vector3 endPosition;
+        public GameObject startSphere;
+        public GameObject endSphere;
+
         void Awake()
         {
             navMeshAgent = GetComponent<NavMeshAgent>();
@@ -18,13 +24,54 @@ namespace roundbeargames_tutorial
 
         public void GoToTarget()
         {
+            startSphere.transform.parent = null;
+            endSphere.transform.parent = null;
+
+            navMeshAgent.isStopped = false;
+
             if (targetPlayableCharacter)
             {
                 target = CharacterManager.instance.GetPlayableCharacter().gameObject;
             }
 
             navMeshAgent.SetDestination(target.transform.position);
+
+            if (move != null)
+            {
+                StopCoroutine(move);
+            }
+
+            move = StartCoroutine(_Move());
+        }
+
+        IEnumerator _Move()
+        {
+            while (true)
+            {
+                if (navMeshAgent.isOnOffMeshLink)
+                {
+                    startPosition = transform.position;
+                    startSphere.transform.position = startPosition;
+                    navMeshAgent.CompleteOffMeshLink();
+
+                    yield return new WaitForEndOfFrame();
+                    endPosition = transform.position;
+                    endSphere.transform.position = endPosition;
+                    navMeshAgent.isStopped = true;
+                    yield break;
+                }
+
+                Vector3 dist = transform.position - navMeshAgent.destination;
+                if (Vector3.SqrMagnitude(dist) < 0.5f)
+                {
+                    startPosition = transform.position;
+                    endPosition = transform.position;
+                    navMeshAgent.isStopped = true;
+                    yield break;
+                }
+
+                yield return new WaitForEndOfFrame();
+            }
         }
     }
-
 }
